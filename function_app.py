04969da6ea12959a3c55d8c03a24d4eeaecbb892
DDTraining2025 <ggwp0818@outlook.com>
2025-06-09 01:14:20 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,16 +1,18 @@
 import azure.functions as func
+from azure.functions import FunctionApp, TimerRequest
 
-# 1) Load secrets at startup
+# load secrets once at startup
 from keyvaultloader import load_secrets_from_vault
 load_secrets_from_vault()
 
-# 2) Import only your Function blueprints
+# import your three blueprints
 from azfuncs.intraday_alert import bp as intraday_bp
 from azfuncs.rss_listener    import bp as rss_bp
 from azfuncs.outcome_tracker import bp as outcome_bp
 
-# 3) Wire them into the FunctionApp
-app = func.FunctionApp()
+app = FunctionApp()
+
+# register the blueprints you defined in azfuncs/*
 app.register_functions(intraday_bp)
 app.register_functions(rss_bp)
 app.register_functions(outcome_bp)
