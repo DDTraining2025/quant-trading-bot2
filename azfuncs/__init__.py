448789f3e7437ae56bca3db7e594d9fd98dba3a6
DDTraining2025 <ggwp0818@outlook.com>
2025-06-09 16:54:09 +0100

    Update __init__.py

--- azfuncs/__init__.py
+++ azfuncs/__init__.py
@@ -2,13 +2,14 @@
 Package for Azure Function blueprints.
 """
 
-# Azure Function Blueprints
 from .intraday_alert import bp as intraday_bp
-from .outcome_tracker import bp as outcome_bp
 from .rss_listener import bp as rss_bp
+from .outcome_tracker import bp as outcome_bp
+from .hello_timer import bp as hello_bp  # 👈 NEW
 
 __all__ = [
     "intraday_bp",
     "outcome_bp",
     "rss_bp",
+    "hello_bp",
 ]
