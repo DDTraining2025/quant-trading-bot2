72075125bb388e1703e0c4e372303d4720b43034
DDTraining2025 <ggwp0818@outlook.com>
2025-06-10 07:31:02 +0100

    Update dbwriter.py

--- shared/dbwriter.py
+++ shared/dbwriter.py
@@ -1,8 +1,8 @@
 import os
-import psycopg2
+import pg8000
 
 def log_alert(ticker, title, url, ts, market_cap):
-    conn = psycopg2.connect(
+    conn = pg8000.connect(
         dbname=os.getenv("pg_db"),
         user=os.getenv("pg_user"),
         password=os.getenv("pg_password"),
