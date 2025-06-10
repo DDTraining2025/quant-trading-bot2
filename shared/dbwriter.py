0c7a1e692a9c3401826a34d7caf4d3badd80e4c6
DDTraining2025 <ggwp0818@outlook.com>
2025-06-10 20:16:29 +0100

    Update dbwriter.py

--- shared/dbwriter.py
+++ shared/dbwriter.py
@@ -3,7 +3,7 @@ import pg8000
 
 def log_alert(ticker, title, url, ts, market_cap):
     conn = pg8000.connect(
-        dbname=os.getenv("pg_database"),
+        database=os.getenv("pg_database"),
         user=os.getenv("pg_user"),
         password=os.getenv("pg_password"),
         host=os.getenv("pg_host"),
